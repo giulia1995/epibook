4c8d47a352e9b92b7dae332dff82dd8caa6434db
Giulia_vi95 <giulia.vaccaro95@gmail.com>
2024-02-18 13:10:22 +0100

    add commenta area, error, loading

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,45 +1,50 @@
 import { Row, Col, Form } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 import { useState } from "react";
+import CommentArea from './CommentArea';
 
-const AllTheBooks=({ books }) => {
+const AllTheBooks = ({ books }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedBook, setSelectedBook] = useState(null);
   const changeSelectedBook = (asin) => {
-    setSelectedBook(asin)};
-    return (
-      <>
-        <Row>
-          <Col>
-            <Row className="mt-5">
-              <Col xs={12} md={4} className="text-center">
-                <Form.Group>
-                  <Form.Control
-                    type="search"
-                    placeholder="Cerca un libro"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
+    setSelectedBook(asin);
+  };
+  return (
+    <>
+      <Row>
+        <Col>
+          <Row className="mt-5">
+            <Col xs={12} md={4} className="text-center">
+              <Form.Group>
+                <Form.Control
+                  type="search"
+                  placeholder="Cerca un libro"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="g-2 mt-3">
+            {books
+              .filter((b) => b.title.toLowerCase().includes(searchQuery))
+              .map((b) => (
+                <Col xs={12} md={4} key={b.asin}>
+                  <SingleBook
+                    book={b}
+                    selectedBook={selectedBook}
+                    changeSelectedBook={changeSelectedBook}
                   />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row className="g-2 mt-3">
-              {books
-                .filter((b) => b.title.toLowerCase().includes(searchQuery))
-                .map((b) => (
-                  <Col xs={12} md={4} key={b.asin}>
-                    <SingleBook
-                      book={b}
-                      selectedBook={selectedBook}
-                      changeSelectedBook={changeSelectedBook}
-                    />
-                  </Col>
-                ))}
-            </Row>
-          </Col>
-        </Row>
-      </>
-    )
+                </Col>
+              ))}
+          </Row>
+        </Col>
+        <Col md={4}>
+          <CommentArea asin={selectedBook} />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default AllTheBooks;
---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,39 @@
+import { Button, ListGroup } from 'react-bootstrap'
+
+const SingleComment = ({ comment }) => {
+  const deleteComment = async (asin) => {
+    try {
+      let response = await fetch(
+        'https://striveschool-api.herokuapp.com/api/comments/' + asin,
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTcyZmVhNmZlMDMxZTAwMTliYTE0ZjUiLCJpYXQiOjE3MDc4NjQ5MzEsImV4cCI6MTcwOTA3NDUzMX0.hNdpCapjgSefC2_EBIe4ia_YyFhIvv45gsc1NRiaAc0",
+          },
+        }
+      )
+      if (response.ok) {
+        alert('La recensione è stata elimata!')
+      } else {
+        throw new Error('La recensione non è stata eliminata!')
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  return (
+    <ListGroup.Item data-testid="single-comment" className='d-flex justify-content-between'>
+      {comment.comment}<br/>valutazione:{comment.rate}
+      <Button
+        variant="danger"
+        className="ms-2"
+        onClick={() => deleteComment(comment._id)}
+      >
+        Elimina
+      </Button>
+    </ListGroup.Item>
+  )
+}
+
+export default SingleComment
